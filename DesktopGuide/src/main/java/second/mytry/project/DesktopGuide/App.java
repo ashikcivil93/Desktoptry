a8a42afe5b247b813e3b72c096d3fe2e681b2bc2
ashikcivil93 <ashikcivil93@gmail.com>
2021-10-18 12:49:19 +0530

    change

---
+++ DesktopGuide/src/main/java/second/mytry/project/DesktopGuide/App.java
@@ -0,0 +1,13 @@
+package second.mytry.project.DesktopGuide;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ DesktopGuide/src/test/java/second/mytry/project/DesktopGuide/AppTest.java
@@ -0,0 +1,21 @@
+package second.mytry.project.DesktopGuide;
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
